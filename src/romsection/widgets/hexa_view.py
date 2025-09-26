fb5e30d92565d45b415179bf15ee4bf379c03047
Valentin Valls <valentin.valls@esrf.fr>
2025-09-26 14:46:29 +0200

    Added hexa view

---
+++ src/romsection/widgets/hexa_view.py
@@ -0,0 +1,168 @@
+from PyQt5 import Qt
+
+
+class HexaTableModel(Qt.QAbstractTableModel):
+    """Table of hexadecimal rendering of byteq data.
+
+    Bytes are displayed one by one as a hexadecimal viewer.
+
+    The 16th first columns display bytes as hexadecimal, the last column
+    displays the same data as ASCII.
+    """
+
+    def __init__(self, parent: Qt.QWidget | None = None):
+        Qt.QAbstractTableModel.__init__(self, parent)
+        self.__data: bytes | None = None
+        self.__address: int = 0
+        self.__start: int = 0
+        self.__length: int = 0
+        self.__padding: int = 0
+        self.__font = Qt.QFontDatabase.systemFont(Qt.QFontDatabase.FixedFont)
+        self.__palette = Qt.QPalette()
+
+    def rowCount(self, parent_idx=None):
+        """Returns number of rows to be displayed in table"""
+        if self.__data is None or self.__length == 0:
+            return 0
+        return ((self.__length - 1) >> 4) + 1
+
+    def columnCount(self, parent_idx=None):
+        """Returns number of columns to be displayed in table"""
+        return 0x10 + 1
+
+    def data(self, index, role=Qt.Qt.DisplayRole):
+        """QAbstractTableModel method to access data values
+        in the format ready to be displayed"""
+        if not index.isValid():
+            return None
+
+        if self.__data is None:
+            return None
+
+        row = index.row()
+        column = index.column()
+
+        if role == Qt.Qt.DisplayRole:
+            if column == 0x10:
+                start = row << 4
+                text = ""
+                for i in range(0x10):
+                    pos = start + i
+                    if pos >= self.__length:
+                        break
+                    if pos < self.__padding:
+                        break
+                    value = self.__data[pos - self.__padding]
+                    if value > 0x20 and value < 0x7F:
+                        text += chr(value)
+                    else:
+                        text += "."
+                return text
+            else:
+                pos = (row << 4) + column
+                if pos < self.__padding:
+                    return ""
+                if pos < self.__length:
+                    value = self.__data[pos - self.__padding]
+                    return f"{value:02X}"
+                else:
+                    return ""
+        elif role == Qt.Qt.FontRole:
+            return self.__font
+
+        elif role == Qt.Qt.BackgroundRole:
+            pos = (row << 4) + column
+            if column != 0x10 and (pos < self.__padding or pos >= self.__length):
+                return self.__palette.color(Qt.QPalette.Disabled, Qt.QPalette.Window)
+            else:
+                return None
+
+        elif role == Qt.Qt.TextAlignmentRole:
+            if column == 0x10:
+                return Qt.Qt.AlignLeft
+            else:
+                return Qt.Qt.AlignCenter
+
+        return None
+
+    def headerData(self, section, orientation, role=Qt.Qt.DisplayRole):
+        """Returns the 0-based row or column index, for display in the
+        horizontal and vertical headers"""
+        if section == -1:
+            # PyQt4 send -1 when there is columns but no rows
+            return None
+
+        if role == Qt.Qt.DisplayRole:
+            if orientation == Qt.Qt.Vertical:
+                address = self.__start + (section << 4)
+                return f"{address:08X}"
+            if orientation == Qt.Qt.Horizontal:
+                if section == 0x10:
+                    return "ASCII"
+                else:
+                    return f"{section:02X}"
+        elif role == Qt.Qt.FontRole:
+            return self.__font
+        elif role == Qt.Qt.TextAlignmentRole:
+            if orientation == Qt.Qt.Vertical:
+                return Qt.Qt.AlignRight
+            if orientation == Qt.Qt.Horizontal:
+                if section == 0x10:
+                    return Qt.Qt.AlignLeft
+                else:
+                    return Qt.Qt.AlignCenter
+        return None
+
+    def flags(self, index):
+        """QAbstractTableModel method to inform the view whether data
+        is editable or not.
+        """
+        row = index.row()
+        column = index.column()
+        pos = (row << 4) + column
+        if column != 0x10 and pos >= self.__length:
+            return Qt.Qt.NoItemFlags
+        return Qt.QAbstractTableModel.flags(self, index)
+
+    def setBytes(self, data: bytes | None, address: int = 0):
+        """Set the data array."""
+        self.beginResetModel()
+        self.__data = data
+        self.__address = address
+        self.__start = (address >> 4) << 4
+        self.__padding = self.__address - self.__start
+        if data is not None:
+            self.__length = self.__padding + len(data)
+        else:
+            self.__length = 0
+        self.endResetModel()
+
+    def bytes(self) -> bytes | None:
+        """Returns the internal data."""
+        return self.__data
+
+
+class HexaView(Qt.QTableView):
+    """TableView using HexaTableModel as default model.
+
+    It customs the column size to provide a better layout.
+    """
+
+    def __init__(self, parent: Qt.QWidget | None = None):
+        Qt.QTableView.__init__(self, parent)
+        model = HexaTableModel(self)
+        self.setModel(model)
+
+    def setData(self, data: bytes | None, address: int=0):
+        """Set the binary data."""
+        self.model().setBytes(data, address=address)
+        self.__fixHeader()
+
+    def __fixHeader(self):
+        """Update the view according to the state of the auto-resize"""
+        header = self.horizontalHeader()
+        header.setDefaultSectionSize(30)
+        header.setStretchLastSection(True)
+        for i in range(0x10):
+            header.setSectionResizeMode(i, Qt.QHeaderView.Fixed)
+        header.setSectionResizeMode(0x10, Qt.QHeaderView.Stretch)
