c2308f71c557d30c4d2e0818ce8f2c07688eed95
Valentin Valls <valentin.valls@esrf.fr>
2025-09-27 22:37:24 +0200

    Added pixel browser

---
+++ src/romsection/widgets/pixel_browser.py
@@ -0,0 +1,128 @@
+import os
+import io
+import numpy
+from PyQt5 import Qt
+
+from ..gba_file import ImageColorMode, ImagePixelOrder
+from .. import array_utils
+from .pixel_browser_widget import PixelBrowserWidget
+from .image_pixel_order_combo import ImagePixelOrderCombo
+from .image_color_mode_combo import ImageColorModeCombo
+
+
+class PixelBrowser(Qt.QFrame):
+    def __init__(self, parent: Qt.QWidget | None = None):
+        Qt.QFrame.__init__(self, parent=parent)
+        self.setFrameShadow(Qt.QFrame.Sunken)
+        self.setFrameShape(Qt.QFrame.StyledPanel)
+
+        self.__widget = PixelBrowserWidget(self)
+        self.__scroll = Qt.QScrollBar(self)
+        self.__scroll.setTracking(True)
+        self.__toolbar = Qt.QToolBar(self)
+
+        self.__zoom = Qt.QSpinBox(self.__toolbar)
+        self.__zoom.setRange(1, 8)
+        self.__zoom.setValue(self.__widget.zoom())
+        self.__toolbar.addWidget(self.__zoom)
+
+        self.__pixelWidth = Qt.QSpinBox(self.__toolbar)
+        self.__pixelWidth.setRange(1, 128)
+        self.__pixelWidth.setValue(self.__widget.pixelWidth())
+        self.__toolbar.addWidget(self.__pixelWidth)
+
+        self.__colorMode = ImageColorModeCombo(self.__toolbar)
+        self.__colorMode.selectValue(self.__widget.colorMode())
+        self.__toolbar.addWidget(self.__colorMode)
+
+        self.__pixelOrder = ImagePixelOrderCombo(self.__toolbar)
+        self.__pixelOrder.selectValue(self.__widget.pixelOrder())
+        self.__toolbar.addWidget(self.__pixelOrder)
+
+        layout = Qt.QGridLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+
+        layout.addWidget(self.__toolbar, 0, 0, 1, 1)
+        layout.addWidget(self.__widget, 1, 0)
+        layout.addWidget(self.__scroll, 1, 1)
+        layout.setRowStretch(1, 1)
+        layout.setColumnStretch(1, 1)
+
+        self.__zoom.valueChanged.connect(self.__zoomChanged)
+        self.__pixelWidth.valueChanged.connect(self.__pixelWidthChanged)
+        self.__colorMode.currentIndexChanged.connect(self.__colorModeChanged)
+        self.__pixelOrder.currentIndexChanged.connect(self.__pixelOrderChanged)
+        self.__scroll.valueChanged.connect(self.__positionChanged)
+
+        shortcut = Qt.QShortcut(Qt.QKeySequence("Left"), self)
+        shortcut.activated.connect(self.moveToPreviousByte)
+        shortcut = Qt.QShortcut(Qt.QKeySequence("Right"), self)
+        shortcut.activated.connect(self.moveToNextByte)
+        shortcut = Qt.QShortcut(Qt.QKeySequence("Up"), self)
+        shortcut.activated.connect(self.moveToPreviousLine)
+        shortcut = Qt.QShortcut(Qt.QKeySequence("Down"), self)
+        shortcut.activated.connect(self.moveToNextLine)
+
+    def moveToPreviousByte(self):
+        pos = self.__widget.position() - 1
+        pos = max(pos, 0)
+        self.__widget.setPosition(pos)
+
+    def moveToNextByte(self):
+        pos = self.__widget.position() + 1
+        pos = min(pos, self.__widget.memoryLength())
+        self.__widget.setPosition(pos)
+
+    def moveToPreviousLine(self):
+        pos = self.__widget.position() - self.__widget.bytesPerLine()
+        pos = max(pos, 0)
+        self.__widget.setPosition(pos)
+
+    def moveToNextLine(self):
+        pos = self.__widget.position() + self.__widget.bytesPerLine()
+        pos = min(pos, self.__widget.memoryLength())
+        self.__widget.setPosition(pos)
+
+    def __zoomChanged(self, zoom: int):
+        self.__widget.setZoom(zoom)
+
+    def __pixelWidthChanged(self, pixelWidth: int):
+        self.__widget.setPixelWidth(pixelWidth)
+
+    def __colorModeChanged(self, index: int):
+        value = self.__colorMode.itemData(index)
+        self.__widget.setColorMode(value)
+
+    def __pixelOrderChanged(self, index: int):
+        value = self.__pixelOrder.itemData(index)
+        self.__widget.setPixelOrder(value)
+
+    def __positionChanged(self, position: int):
+        self.__widget.setPosition(position)
+
+    def pixelWidth(self) -> int:
+        return self.__widget.pixelWidth()
+
+    def setPixelWidth(self, width: int):
+        return self.__widget.setPixelWidth(width)
+
+    def memory(self) -> io.IOBase:
+        return self.__widget.memory()
+
+    def setMemory(self, memory: io.IOBase):
+        self.__widget.setMemory(memory)
+        self.__scroll.setValue(0)
+        self.__scroll.setRange(0, self.__widget.memoryLength())
+
+    def colorMode(self) -> ImageColorMode:
+        return self.__widget.colorMode()
+
+    def setColorMode(self, colorMode: ImageColorMode):
+        self.__widget.setColorMode(colorMode)
+
+    def pixelOrder(self) -> ImagePixelOrder:
+        return self.__widget.pixelOrder()
+
+    def setPixelOrder(self, pixelOrder: ImagePixelOrder):
+        self.__widget.setPixelOrder(pixelOrder)
---
+++ src/romsection/widgets/pixel_browser_widget.py
@@ -0,0 +1,275 @@
+import os
+import io
+import numpy
+from PyQt5 import Qt
+
+from ..gba_file import ImageColorMode, ImagePixelOrder
+from .. import array_utils
+
+
+class PixelBrowserWidget(Qt.QWidget):
+    def __init__(self, parent: Qt.QWidget | None = None):
+        Qt.QWidget.__init__(self, parent=parent)
+        self.__colorMode = ImageColorMode.INDEXED_8BIT
+        self.__pixelOrder = ImagePixelOrder.NORMAL
+        self.__memory: io.IOBase = io.BytesIO(b"")
+        self.__pos: int = 0
+        self.__len: int = 0
+        self.__pixelWidth: int = 48
+        self.__zoom: int = 4
+
+    def zoom(self) -> int:
+        return self.__zoom
+
+    def setZoom(self, zoom: int):
+        if zoom == self.__zoom:
+            return
+        self.__zoom = zoom
+        self.update()
+
+    def position(self) -> int:
+        return self.__pos
+
+    def setPosition(self, position: int):
+        if position == self.__pos:
+            return
+        self.__pos = position
+        self.update()
+
+    def pixelWidth(self) -> int:
+        return self.__pixelWidth
+
+    def setPixelWidth(self, pixelWidth: int):
+        if pixelWidth == self.__pixelWidth:
+            return
+        self.__pixelWidth = pixelWidth
+        self.update()
+
+    def memory(self) -> io.IOBase:
+        return self.__memory
+
+    def setMemory(self, memory: io.IOBase):
+        if self.__memory == memory:
+            return
+        self.__memory = memory
+
+        self.__memory.seek(0, os.SEEK_END)
+        self.__len = self.__memory.tell()
+        self.__memory.seek(0, os.SEEK_SET)
+        self.__pos = 0
+
+        self.update()
+
+    def memoryLength(self) -> int:
+        return self.__len
+
+    def colorMode(self) -> ImageColorMode:
+        return self.__colorMode
+
+    def setColorMode(self, colorMode: ImageColorMode):
+        if self.__colorMode == colorMode:
+            return
+        self.__colorMode = colorMode
+        self.update()
+
+    def pixelOrder(self) -> ImagePixelOrder:
+        return self.__pixelOrder
+
+    def setPixelOrder(self, pixelOrder: ImagePixelOrder):
+        if self.__pixelOrder == pixelOrder:
+            return
+        self.__pixelOrder = pixelOrder
+        self.update()
+
+    def _readBytes(self, pos: int, length: int) -> bytes:
+        """
+        Read an amount of bytes.
+
+        The amount of bytes can differ from the request if the
+        steam is empty.
+        """
+        self.__memory.seek(pos, os.SEEK_SET)
+        return self.__memory.read(length)
+
+    def _getNbBytesPerPixels(self, nb_pixels: int) -> int:
+        """Return the minimal nb bytes mandatory to display `nb_pixels`."""
+        if self.__pixelOrder == ImagePixelOrder.NORMAL:
+            pass
+        elif self.__pixelOrder == ImagePixelOrder.TILED_8X8:
+            # Have to be aligned to the next 8x8 pixels
+            remained = nb_pixels % 8 * 8
+            if remained != 0:
+                nb_pixels += 8 * 8 - remained
+        else:
+            raise ValueError(f"Unsupported {self.__pixelOrder}")
+
+        if self.__colorMode == ImageColorMode.INDEXED_8BIT:
+            result = nb_pixels
+        elif self.__colorMode == ImageColorMode.INDEXED_4BIT:
+            remainded = nb_pixels % 2
+            result = nb_pixels // 2
+            if remainded != 0:
+                # Have to be aligned to the next 2 pixels
+                result += 1
+        elif self.__colorMode == ImageColorMode.A1RGB15:
+            result = nb_pixels * 2
+        elif self.__colorMode == ImageColorMode.RGB15:
+            result = nb_pixels * 2
+        else:
+            raise ValueError(f"Unsupported {self.__colorMode}")
+
+        return result
+
+    def _getConstrainedHeight(self, height: int) -> int:
+        """
+        Use the codec constraint to allow to display at least this `height`.
+        """
+        if self.__pixelOrder == ImagePixelOrder.TILED_8X8:
+            remaining = height % 8
+            if remaining != 0:
+                height += 8 - remaining
+        return height
+
+    def _getConstrainedWidth(self, width: int) -> int:
+        """
+        Use the codec constraint to allow to display at least this `width`.
+        """
+        if self.__pixelOrder == ImagePixelOrder.TILED_8X8:
+            remaining = width % 8
+            if remaining != 0:
+                width += 8 - remaining
+        if self.__colorMode == ImageColorMode.INDEXED_4BIT:
+            # Only display lines aligned to a byte
+            if width % 2 != 0:
+                width += 1
+        return width
+
+    def _getNbBytesForEasyDisplay(self, max_bytes: int, width: int) -> int:
+        """
+        Return the nb bytes which are easy to display.
+
+        It's the minimal nb bytes, smaller or equal than this `nb_bytes`,
+        which can be used to dispklay a full rectangle of the requested
+        `width`.
+        """
+        if self.__pixelOrder == ImagePixelOrder.NORMAL:
+            nb_pixels_for_width = width
+        elif self.__pixelOrder == ImagePixelOrder.TILED_8X8:
+            # The width should already be properly constrainted
+            assert width % 8 == 0
+            nb_tile_per_width = width // 8
+            nb_pixels_for_width = nb_tile_per_width * 8 * 8
+        else:
+            raise ValueError(f"Unsupported {self.__pixelOrder}")
+
+        if self.__colorMode == ImageColorMode.INDEXED_8BIT:
+            nb_bytes_for_width = nb_pixels_for_width
+        elif self.__colorMode == ImageColorMode.INDEXED_4BIT:
+            # The width should already be properly constrainted
+            assert width % 2 == 0
+            nb_bytes_for_width = nb_pixels_for_width // 2
+        elif self.__colorMode == ImageColorMode.A1RGB15:
+            nb_bytes_for_width = nb_pixels_for_width * 2
+        elif self.__colorMode == ImageColorMode.RGB15:
+            nb_bytes_for_width = nb_pixels_for_width * 2
+        else:
+            raise ValueError(f"Unsupported {self.__colorMode}")
+
+        nb_bytes = max_bytes - max_bytes % nb_bytes_for_width
+        return nb_bytes
+
+    def paintEvent(self, event: Qt.QPaintEvent):
+        painter = Qt.QPainter(self)
+        self._paintAll(painter)
+
+    def _getBytesPerLine(self, width: int) -> int:
+        if self.__colorMode == ImageColorMode.INDEXED_8BIT:
+            return width
+        elif self.__colorMode == ImageColorMode.INDEXED_4BIT:
+            assert width % 2 == 0
+            return width // 2
+        elif self.__colorMode == ImageColorMode.A1RGB15:
+            return width * 2
+        elif self.__colorMode == ImageColorMode.RGB15:
+            return width * 2
+        raise ValueError(f"Unsupported {self.__colorMode}")
+
+    def bytesPerLine(self) -> int:
+        width = self._getConstrainedWidth(self.__pixelWidth)
+        return self._getBytesPerLine(width)
+
+    def _toImage(self, data: bytes, width: int) -> Qt.QImage:
+        if len(data) == 0:
+            return Qt.QImage()
+
+        if self.__pixelOrder == ImagePixelOrder.NORMAL:
+            pass
+
+        bytes_per_line = self._getBytesPerLine(width)
+        height = len(data) // bytes_per_line
+
+        # FIXME: Some cases can have shortcuts
+
+        if self.__colorMode == ImageColorMode.INDEXED_8BIT:
+            array = numpy.frombuffer(data, dtype=numpy.uint8)
+        elif self.__colorMode == ImageColorMode.INDEXED_4BIT:
+            array = numpy.frombuffer(data, dtype=numpy.uint8)
+            array = array_utils.convert_8bx1_to_4bx2(array)
+            # FIXME: While there is no palette
+            array *= 0xF
+        elif self.__colorMode == ImageColorMode.A1RGB15:
+            array = numpy.frombuffer(data, dtype=numpy.uint16)
+            array = array_utils.convert_a1rgb15_to_argb32(array, use_alpha=True)
+        elif self.__colorMode == ImageColorMode.RGB15:
+            array = numpy.frombuffer(data, dtype=numpy.uint16)
+            array = array_utils.convert_a1rgb15_to_argb32(array, use_alpha=False)
+        else:
+            raise ValueError(f"Unsupported {self.__colorMode}")
+
+        if self.__pixelOrder == ImagePixelOrder.TILED_8X8:
+            array.shape = height, width, -1
+            array = array_utils.convert_to_tiled_8x8(array)
+
+        if self.__colorMode in [ImageColorMode.INDEXED_8BIT, ImageColorMode.INDEXED_4BIT]:
+            image = Qt.QImage(
+                array.tobytes(),
+                width,
+                height,
+                Qt.QImage.Format_Grayscale8,
+            )
+        else:
+            image = Qt.QImage(
+                array.tobytes(),
+                width,
+                height,
+                Qt.QImage.Format_ARGB32,
+            )
+        return image
+
+    def _paintAll(self, painter: Qt.QPainter):
+        painter.save()
+
+        transform = Qt.QTransform()
+        transform = transform.scale(self.__zoom, self.__zoom)
+        painter.setTransform(transform)
+
+        visibleHeight = self.height() // self.__zoom
+        if self.height() % self.__zoom != 0:
+            visibleHeight += 1
+
+        height = self._getConstrainedHeight(visibleHeight)
+        width = self._getConstrainedWidth(self.__pixelWidth)
+
+        nb_pixels = width * height
+        nb_bytes = self._getNbBytesPerPixels(nb_pixels)
+
+        binaryData = self._readBytes(self.__pos, nb_bytes)
+        nbEasyBytes = self._getNbBytesForEasyDisplay(len(binaryData), width)
+        easyBytes = binaryData[0:nbEasyBytes]
+        image = self._toImage(easyBytes, width)
+        painter.drawImage(Qt.QPoint(0, 0), image)
+
+        remainingBytes = binaryData[nbEasyBytes:]
+        # FIXME: Draw the remaining stuff
+
+        painter.restore()
