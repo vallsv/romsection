7fdaafecb2672f7309447b8391095144617d6463
Valentin Valls <valentin.valls@esrf.fr>
2025-10-05 19:11:00 +0200

    Created convert_range_to_uint8

--- tests/test_array_utils.py
+++ tests/test_array_utils.py
@@ -1,5 +1,7 @@
+import pytest
 import numpy
 from romsection.array_utils import convert_8bx1_to_4bx2, convert_to_tiled_8x8, convert_16bx1_to_5bx3
+from romsection import array_utils
 
 
 def test_convert_8bx1_to_4bx2():
@@ -24,3 +26,29 @@ def test_convert_16bx1_to_5bx3():
     source = numpy.array([0b0111111111011000], numpy.uint16)
     result = convert_16bx1_to_5bx3(source)
     numpy.testing.assert_allclose(result, expected)
+
+
+@pytest.mark.parametrize(
+    "array,expected",
+    (
+        (
+            numpy.array([0, 127, 255], dtype=numpy.uint8),
+            numpy.array([0, 127, 255], dtype=numpy.uint8),
+        ),
+        (
+            numpy.array([-128, 0, 127], dtype=numpy.int8),
+            numpy.array([0, 128, 255], dtype=numpy.uint8),
+        ),
+        (
+            numpy.array([0, 128 * 256, 255 * 256], dtype=numpy.uint16),
+            numpy.array([0, 128, 255], dtype=numpy.uint8),
+        ),
+        (
+            numpy.array([-128 * 256, 0, 127 * 256], dtype=numpy.int16),
+            numpy.array([0, 128, 255], dtype=numpy.uint8),
+        ),
+    ),
+)
+def test_translate_range_to_uint8(array, expected):
+    result = array_utils.translate_range_to_uint8(array)
+    numpy.testing.assert_equal(result, expected)
