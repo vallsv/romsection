c77b9df81f8850c626f7e6788368032007033052
Valentin Valls <valentin.valls@esrf.fr>
2025-10-18 10:47:10 +0200

    Added entry point

--- src/romsection/__main__.py
+++ src/romsection/__main__.py
@@ -5,33 +5,38 @@ from importlib.metadata import version
 from .gba_file import GBAFile
 
 
-parser = argparse.ArgumentParser(
-    prog='RomSection',
-    description='Dissect GameBoyAdvance ROMs',
-    epilog='Text at the bottom of help')
+def main():
+    parser = argparse.ArgumentParser(
+        prog='RomSection',
+        description='Dissect GameBoyAdvance ROMs',
+    )
 
-parser.add_argument(
-    "filename",
-    nargs='?',
-    help="Filename of a .gba ROM file"
-)
-parser.add_argument(
-    "--version",
-    action="version",
-    version=f"%(prog)s {version('romsection')}"
-)
+    parser.add_argument(
+        "filename",
+        nargs='?',
+        help="Filename of a .gba ROM file or .toml file"
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"%(prog)s {version('romsection')}"
+    )
 
-args = parser.parse_args()
-app = Qt.QApplication([])
+    args = parser.parse_args()
+    app = Qt.QApplication([])
 
-from . import resources
-resources.initResources()
+    from . import resources
+    resources.initResources()
 
-from .extractor import Extractor
-win = Extractor()
+    from .extractor import Extractor
+    win = Extractor()
 
-if args.filename is not None:
-    win.loadFilename(args.filename)
+    if args.filename is not None:
+        win.loadFilename(args.filename)
 
-win.show()
-app.exec()
+    win.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
